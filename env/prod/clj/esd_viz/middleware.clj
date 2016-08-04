835d53aeb1f3b88f0ae906bc0201f3cde97d94e6
Ethan Miller  - Development Server <ethanzanemiller@gmail.com>
2016-08-04 21:36:37 +0100

    Add ring-json middleware to prod env

--- env/prod/clj/esd_viz/middleware.clj
+++ env/prod/clj/esd_viz/middleware.clj
@@ -1,5 +1,9 @@
 (ns esd-viz.middleware
-  (:require [ring.middleware.defaults :refer [site-defaults wrap-defaults]]))
+  (:require [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
+            [ring.middleware.json :as json-middleware]))
 
 (defn wrap-middleware [handler]
-  (wrap-defaults handler site-defaults))
+  (-> handler
+        (wrap-defaults site-defaults)
+        (json-middleware/wrap-json-body {:keywords? true})
+        json-middleware/wrap-json-response))
